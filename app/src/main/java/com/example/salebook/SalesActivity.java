03c3d69ddb69e793921ee9d79d48d6720e4daf29
linhcode2002 <linhho.linhcangfc@gmail.com>
2023-08-09 14:48:09 +0700

    Xử lý nút đăng nhập

---
+++ app/src/main/java/com/example/salebook/SalesActivity.java
@@ -0,0 +1,27 @@
+package com.example.salebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class SalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales);
+
+    }
+
+    public void Anhxa(){
+//        btnRegister = (Button)findViewById(R.id.buttonRegister);
+//        cbShowPassword = (CheckBox) findViewById(R.id.cb_show_password);
+//        etUsername = (EditText) findViewById(R.id.editTextTextEmailAddress);
+//        etPassword = (EditText) findViewById(R.id.editTextTextPassword);
+
+    }
+}
