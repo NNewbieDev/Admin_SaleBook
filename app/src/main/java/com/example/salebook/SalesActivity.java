cbc049169cb2382b045f1adcda0d47dda3026fe9
quanminh010302 <quanminh@gmail.com>
2023-08-13 18:33:58 +0700

    Dang test

--- app/src/main/java/com/example/salebook/SalesActivity.java
+++ app/src/main/java/com/example/salebook/SalesActivity.java
@@ -1,19 +1,46 @@
 package com.example.salebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import com.example.salebook.adapter.ProductAdapter;
+import com.example.salebook.database.DatabaseAdapter;
+import com.example.salebook.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SalesActivity extends AppCompatActivity {
+//    private RecyclerView rclProductList;
+//    private ProductAdapter productAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
+
+//        rclProductList = findViewById(R.id.rclproductlist);
+//
+//
+//        rclProductList.setHasFixedSize(true);
+//
+//
+//        //---- Grid ----
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,3);
+//        gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
+//        rclProductList.setLayoutManager(gridLayoutManager);
+//        rclProductList.setAdapter(productAdapter);
+
+
+
 
     }
 
---
+++ app/src/main/java/com/example/salebook/adapter/ProductAdapter.java
@@ -0,0 +1,70 @@
+package com.example.salebook.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.salebook.R;
+import com.example.salebook.model.Book;
+
+import java.util.ArrayList;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+    private ArrayList<Book> listProduct;
+    private Context context;
+
+    public ProductAdapter(ArrayList<Book> listProduct, Context context) {
+        this.listProduct = listProduct;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txttitle.setText(listProduct.get(position).getTitle());
+        holder.txtprice.setText(String.valueOf(listProduct.get(position).getPrice()));
+//        holder.imgproduct.setImageResource(listProduct.get(position).getImage());
+        holder.btndetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listProduct.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView txttitle, txtprice;
+        public Button btndetail;
+//        public ImageView imgproduct;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txttitle=itemView.findViewById(R.id.txt_title);
+            txtprice = itemView.findViewById(R.id.txt_price);
+            btndetail = itemView.findViewById(R.id.btndetail);
+//            imgproduct = itemView.findViewById(R.id.img_product);
+        }
+    }
+}
